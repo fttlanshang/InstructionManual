88cc8020a6983ccd19497ada15fc60b08e5f9aa2
Tiantian Fu <tian@Tiantians-MacBook-Air.local>
2024-06-17 21:55:30 -0700

    initial commit

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,63 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import instructionmanual.composeapp.generated.resources.Res
+import instructionmanual.composeapp.generated.resources.compose_multiplatform
+import kotlinx.datetime.Clock
+import kotlinx.datetime.IllegalTimeZoneException
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
+@Composable
+fun App() {
+    MaterialTheme {
+        var location by remember { mutableStateOf("Europe/Paris") }
+        var timeAtLocation by remember { mutableStateOf("No location selected") }
+
+        Column {
+            Text(timeAtLocation)
+            TextField(value = location, onValueChange = { location = it })
+            Button(onClick = { timeAtLocation = currentTimeAt(location) ?: "Invalid Location" }) {
+                Text("Show Time At Location")
+            }
+        }
+    }
+}
+
+fun todaysDate(): String {
+    fun LocalDateTime.format() = toString().substringBefore('T')
+
+    val now = Clock.System.now()
+    val zone = TimeZone.currentSystemDefault()
+    return now.toLocalDateTime(zone).format()
+}
+
+fun currentTimeAt(location: String): String? {
+    fun LocalTime.formatted() = "$hour:$minute:$second"
+
+    return try {
+        val time = Clock.System.now()
+        val zone = TimeZone.of(location)
+        val localTime = time.toLocalDateTime(zone).time
+        "The time in $location is ${localTime.formatted()}"
+    } catch (ex: IllegalTimeZoneException) {
+        null
+    }
+}
